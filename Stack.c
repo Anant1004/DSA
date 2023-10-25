ec507fc6aca4b5eeea3ae746cb03c2c1f36a3216
Anant kumar <anant1042005@gmail.com>
2023-10-25 19:53:15 +0530

    completed the stack

--- Stack.c
+++ Stack.c
@@ -27,10 +27,23 @@ int isFull (struct stack *ptr){
 void push (struct stack*ptr,int val){
     if(isFull(ptr)){
         printf("Stack overflow\n");
+        printf("cannot push %d to the stack\n",val);
     }
     else{
         ptr->top++;
         ptr->arr[ptr->top] = val;
+    }
+}
+int pop (struct stack *ptr){
+    if(isEmpty(ptr)){
+        printf("Stack underflow");
+        printf("Cannot pop from the stack");
+        return -1;
+    }
+    else{
+        int val = ptr->arr[ptr->top];
+        ptr->top--;
+        return val;
     }
 }
 
@@ -40,22 +53,28 @@ int main(){
     s->top = -1;
     s->arr = (int *)malloc(s->size * sizeof(int));
 
+    push(s,10);
+    push(s,9);
+    push(s,8);
+    push(s,7);
+    push(s,6);
+    push(s,5);
+    push(s,4);
+    push(s,3);
+    push(s,2);
     push(s,1);
-    push(s,1);
-    push(s,1);
-    push(s,1);
-    push(s,1);
-    push(s,1);
-    push(s,1);
-    push(s,1);
-    push(s,1);
-    push(s,1);
+
+    printf("popped the first item you pushed : %d \n",pop(s));
+    printf("popped the first item you pushed : %d \n",pop(s));
+    printf("popped the first item you pushed : %d \n",pop(s));
+    printf("popped the first item you pushed : %d \n",pop(s));
+
 
     if(isEmpty(s)){
         printf("The stack in empty\n");
     }
     else{
-        printf("The stack not empty\n");
+        printf("The stack is full\n");
     }
     return 0;
 }
